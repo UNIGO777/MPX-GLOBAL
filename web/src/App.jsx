ed3502021f275e40712afe9cd67cd7d348ce57ef
UNIGO777 <naman13399@gmail.com>
2026-08-05 01:33:35 +0530

    asdf

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -66,6 +66,7 @@ function ChunkFallback() {
  * M1 set render the designed ComingSoon page (logged in docs/UiWebNotes.md).
  */
 export function App() {
+  // console.log("asdf")
   return (
     <AuthProvider>
       <BrowserRouter>
